d20d30b7ff8a7f8dcdaf83af7c6cf93acc8adefd
Morten <mitmme16@student.aau.dk>
2020-01-23 18:37:13 +0100

    temp delete

--- fristrupminde-api-backend/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Microsoft.AspNetCore.Identity;
-
-namespace fristrupminde_api_backend.Models
-{
-    public class ApplicationUser : IdentityUser<int>
-    {
-    }
-}
--- fristrupminde-api-backend/Models/ProjectTaskUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace fristrupminde_api_backend.Models
-{
-    public class ProjectTaskUser
-    {
-        public int ProjectTaskID { get; set; }
-        public int UserID { get; set; }
-    }
-}
