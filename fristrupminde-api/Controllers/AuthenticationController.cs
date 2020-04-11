f5aed8c7d0eb32f87b5da29303aad608ec56d4b1
Morten <mitmme16@student.aau.dk>
2020-04-11 23:21:27 +0200

    verify jwt token

--- fristrupminde-api/Controllers/AuthenticationController.cs
+++ fristrupminde-api/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using fristrupminde_api.Models.Inputs.Authentication;
@@ -12,7 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System.Web.Http.Cors;
 using Microsoft.IdentityModel.Tokens;
-
+using System.Security.Principal;
 
 namespace fristrupminde_api.Controllers
 {
@@ -67,13 +69,30 @@ namespace fristrupminde_api.Controllers
             throw new ApplicationException("UNKNOWN_ERROR");
         }
 
+
+        [Route("api/user/validate")]
+        [HttpPost]
+        [EnableCors(origins: "*", headers: "*", methods: "*", SupportsCredentials = true)]
+        public async Task<IActionResult> ValidateUserToken(string token)
+        {
+            if (token != null)
+            {
+                if (ValidateToken(token))
+                {
+                    return Ok();
+                }
+            }
+            return Unauthorized();
+        }
+
+
         private async Task<Object> GenerateJwtToken(ApplicationUser user)
         {
             var claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                //new Claim(ClaimTypes.NameIdentifier, user.Email)
+                new Claim(ClaimTypes.NameIdentifier, user.UserName)
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
@@ -91,5 +110,33 @@ namespace fristrupminde_api.Controllers
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
+
+        private bool ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = GetValidationParameters();
+
+            SecurityToken validatedToken;
+            try
+            {
+                tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private TokenValidationParameters GetValidationParameters()
+        {
+            return new TokenValidationParameters()
+            {
+                ValidIssuer = _config["Jwt:Issuer"],
+                ValidAudience = _config["Jwt:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])),
+                ClockSkew = TimeSpan.Zero // remove delay of token when expire generate the token
+            };
+        }
     }
 }
